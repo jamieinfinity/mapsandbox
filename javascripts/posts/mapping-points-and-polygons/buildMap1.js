d7e06309dccb12261d3c9e2d15a14e8de2bb3c0f
Jamie Williams <jamieinfinity@mac.com>
2013-12-25 20:48:52 -0600

    Site updated at 2013-12-26 02:48:52 UTC

---
+++ javascripts/posts/mapping-points-and-polygons/buildMap1.js
@@ -0,0 +1,71 @@
+// the variable mapData is defined in ./dataMap1.js, which must be loaded in web page
+// I'd rather load the data directly in this file, but haven't figure out how to do that
+
+var geoJson;
+var map;
+
+function pointStyle(feature) {
+    return {
+        fillColor: feature.properties.color,
+        color: feature.properties.color,
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.3
+    }
+};
+
+function polygonStyle(feature) {
+    return {
+        fillColor: "#00aa33",
+        color: "#00aa33",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.3
+    }
+};
+
+function highlightFeature(event) {
+    var layer = event.target;
+
+    layer.setStyle({
+        fillOpacity: .7
+    });
+
+    var popup = L.popup({closeButton: false}, layer)
+        .setLatLng(event.latlng)
+        .setContent(layer.feature.properties.description)
+        .openOn(map);
+}
+
+function resetHighlight(event) {
+    var layer = event.target;
+    geoJson.resetStyle(layer);
+    map.closePopup();
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight
+    });
+}
+
+var map = L.mapbox.map('mbmap', 'examples.map-20v6611k', {
+    scrollWheelZoom: false
+});
+map.setView(new L.LatLng(38.6288, -90.193688), 16);
+
+var geoJson = L.geoJson(mapData, {
+    pointToLayer: function (feature, latlng) {
+        return L.circle(latlng, 20);
+    },
+    style: function (feature) {
+        switch (feature.geometry.type) {
+            case 'Point':
+                return pointStyle(feature);
+            case 'Polygon':
+                return polygonStyle(feature);
+        }
+    },
+    onEachFeature: onEachFeature
+}).addTo(map);
---
+++ javascripts/posts/mapping-points-and-polygons/dataMap1.js
@@ -0,0 +1,26 @@
+var mapData = { "type": "FeatureCollection",
+    "features": [
+        { "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-90.193253, 38.631289]},
+            "properties": {"description": "new T-REX location", "color": "#3a38bf"}
+        },
+        { "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-90.1907, 38.62844]},
+            "properties": {"description": "old T-REX location", "color": "#da1337"}
+        },
+        { "type": "Feature",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [-90.196158, 38.62781],
+                        [-90.193031, 38.627043],
+                        [-90.193368, 38.626247],
+                        [-90.19641, 38.626997]
+                    ]
+                ]
+            },
+            "properties": { "description": "City Garden" }
+        }
+    ]
+};
